fd8f6722d4d63b366b505ab1f5aeb93f6f06ac28
bimmbin <bimmbin.gomez@gmail.com>
2023-09-30 17:34:27 +0800

    mobile responsive: herosection

--- resources/js/Components/HomeComponents/ScrollTools.vue
+++ resources/js/Components/HomeComponents/ScrollTools.vue
@@ -14,6 +14,6 @@ const path1 = ref("/img/");
 
 <template>
     <div class="flex items-center gap-2">
-        <img :src="path1 + props.imageSrc" class="inline-block h-14" />
+        <img :src="path1 + props.imageSrc" class="inline-block h-14 max-lg:h-10" />
     </div>
 </template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -3,17 +3,18 @@ import { ref } from "vue";
 import { Link } from '@inertiajs/vue3';
 
 const logo = ref("/img/logo.png");
+const burger = ref("/img/burger.svg");
 </script>
 
 <template>
     <div class="font-pop bg-portbg text-white w-screen">
-        <div class="w-[1200px] mx-auto">
+        <div class="w-[1200px] mx-auto max-xl:w-[800px] max-md:w-screen max-md:px-5">
             <!-- Header -->
             <div
                 class="flex justify-between items-center bg-portbg h-20 text-white"
             >
                 <img :src="logo" alt="" class="h-8" />
-                <nav>
+                <nav class="max-lg:hidden">
                     <ul class="flex gap-10">
                         <Link :href="route('home')">Home</Link>
                         <Link :href="route('home')">Projects</Link>
@@ -22,10 +23,13 @@ const logo = ref("/img/logo.png");
                         <li>Contact</li>
                     </ul>
                 </nav>
-                <div class="">
+                <div class="max-lg:hidden">
                     <p class="border border-main py-1.5 px-5 rounded-full">
                         Login
                     </p>
+                </div>
+                <div class="lg:hidden">
+                  <img :src="burger" alt="" class="h-6 " />
                 </div>
             </div>
 
